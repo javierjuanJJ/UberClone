43b9fd8ca4d563dcea3220df52b5a43c8625fc7a
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-10-10 11:19:43 +0200

    User Login and Registration

---
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/uberclone/CustomerLoginActivity.java
@@ -0,0 +1,114 @@
+package whatsappclone.proyecto_javier_juan_uceda.uberclone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class CustomerLoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnLogin, btnRegister;
+    private EditText etEmail, etPassword;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_login);
+
+        setUI();
+
+    }
+
+    private void setUI() {
+        btnLogin = findViewById(R.id.login);
+        btnRegister = findViewById(R.id.register);
+
+        etEmail = findViewById(R.id.email);
+        etPassword = findViewById(R.id.password);
+
+        btnLogin.setOnClickListener(this);
+        btnRegister.setOnClickListener(this);
+
+        etEmail.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        etPassword.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(CustomerLoginActivity.this, MapActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        final String email = etEmail.getText().toString();
+        final String password = etPassword.getText().toString();
+
+        switch (view.getId()){
+            case R.id.login:
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(CustomerLoginActivity.this, getString(R.string.signInError), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                break;
+            case R.id.register:
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(CustomerLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()){
+                            Toast.makeText(CustomerLoginActivity.this, getString(R.string.sinUpError), Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            String userId = mAuth.getCurrentUser().getUid();
+                            DatabaseReference currentUserDB = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child(userId);
+                            currentUserDB.setValue(true);
+                        }
+                    }
+                });
+                break;
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(firebaseAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(firebaseAuthListener);
+    }
+}
--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/uberclone/MainActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/uberclone/MainActivity.java
@@ -2,13 +2,45 @@ package whatsappclone.proyecto_javier_juan_uceda.uberclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnDriver, btnCustomer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUI();
+    }
+
+    private void setUI() {
+        btnCustomer = findViewById(R.id.customer);
+        btnDriver = findViewById(R.id.driver);
+
+        btnCustomer.setOnClickListener(this);
+        btnDriver.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.customer:
+                goToScreen(MainActivity.this,CustomerLoginActivity.class);
+                break;
+            case R.id.driver:
+                goToScreen(MainActivity.this,DriverLoginActivity.class);
+                break;
+        }
+    }
+
+    private void goToScreen(android.content.Context activity,  Class<?> destinatation) {
+        Intent intent = new Intent(activity, destinatation);
+        startActivity(intent);
+        finish();
     }
 }
