4bfb0aad9cff786743ab764f07c75153abdc4e69
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-10-11 14:28:24 +0200

    Saving User Profile Image In Firebase

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/uberclone/CostumerSettingsActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/uberclone/CostumerSettingsActivity.java
@@ -1,21 +1,36 @@
 package whatsappclone.proyecto_javier_juan_uceda.uberclone;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,7 +42,10 @@ public class CostumerSettingsActivity extends AppCompatActivity implements View.
     private FirebaseAuth mAuth;
     private DatabaseReference mCustomerReference;
 
-    private String userId, mName, mPhone;
+    private String userId, mName, mPhone, mProfilePicture;
+
+    private ImageView ivProfilePhoto;
+    private Uri resultUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +61,11 @@ public class CostumerSettingsActivity extends AppCompatActivity implements View.
         etName = findViewById(R.id.etName);
         etPhone = findViewById(R.id.etPhone);
 
+        ivProfilePhoto = findViewById(R.id.profilePhoto);
+
         btnBack.setOnClickListener(this);
         btnConfirm.setOnClickListener(this);
+        ivProfilePhoto.setOnClickListener(this);
 
         mAuth = FirebaseAuth.getInstance();
         userId = mAuth.getCurrentUser().getUid();
@@ -56,6 +77,16 @@ public class CostumerSettingsActivity extends AppCompatActivity implements View.
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            final Uri uri = data.getData();
+            resultUri = uri;
+            ivProfilePhoto.setImageURI(resultUri);
+        }
+    }
+
+    @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btnBack:
@@ -63,6 +94,11 @@ public class CostumerSettingsActivity extends AppCompatActivity implements View.
                 break;
             case R.id.btnConfirm:
                 saveUserInformation();
+                break;
+            case R.id.profilePhoto:
+                Intent image = new Intent(Intent.ACTION_PICK);
+                image.setType("image/*");
+                startActivityForResult(image,1);
                 break;
         }
     }
@@ -83,6 +119,12 @@ public class CostumerSettingsActivity extends AppCompatActivity implements View.
                     if (map.get("phone") != null){
                         mPhone = map.get("phone").toString();
                         etPhone.setText(mPhone);
+                    }
+
+                    if (map.get("profileImageUrl") != null){
+                        mProfilePicture = map.get("profileImageUrl").toString();
+                        Glide.with(getApplication()).load(mProfilePicture).apply(new RequestOptions().override(10, 10)).into(ivProfilePhoto);
+
                     }
                     Log.i("firebase", "Done");
                 }
@@ -105,7 +147,43 @@ public class CostumerSettingsActivity extends AppCompatActivity implements View.
 
         mCustomerReference.updateChildren(userInfo);
 
-        finish();
+        if (resultUri != null) {
+            StorageReference filePath = FirebaseStorage.getInstance().getReference().child("profile_pictures").child(userId);
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(),resultUri);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG,20,baos);
+            byte[] data = baos.toByteArray();
+            UploadTask uploadTask = filePath.putBytes(data);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    finish();
+                }
+            });
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getUploadSessionUri();
+                    HashMap map = new HashMap();
+                    map.put("profileImageUrl",downloadUrl.toString());
+                    mCustomerReference.updateChildren(map);
+                    finish();
+                }
+            });
+
+
+        }
+        else {
+            finish();
+        }
+
+
 
     }
 }
